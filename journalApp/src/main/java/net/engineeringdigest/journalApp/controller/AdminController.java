81bb6dcea1eb4b0014a36942d3b86a027ff6eeb0
ShauryaStar-ai <shauryahooda2009@gmail.com>
2025-12-10 16:22:49 -0500

    even for admin controller I now have set up the meathod in which there are only entry with that, user name

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controller/AdminController.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/AdminController.java
@@ -1,16 +1,18 @@
 package net.engineeringdigest.journalApp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpResponse;
 import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
+@Slf4j
 public class AdminController {
 
     @Autowired
@@ -30,7 +32,18 @@ public class AdminController {
     }
     //Post mapping
     @PostMapping("create-new-admin")
-    public void createAdmin(@RequestBody User user){
+    public ResponseEntity<String> createAdmin(@RequestBody User user){
+
+        if(userService.findByUserName(user.getUserName()) != null){
+            log.debug("Entry already exists.Killing all bugs");
+            log.trace("this is trace over here ");
+            log.error("Entry already exists for {}",user.getUserName());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username already exists");
+        }
+        else{
         userService.saveNewAdmin(user);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Added the new Admin");
+        }
     }
 }
